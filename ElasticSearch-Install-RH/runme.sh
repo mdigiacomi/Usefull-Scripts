5bfe5e1ea78387ca6d636abcaf6e1dca131a33cd
Michael Di Giacomi <Michael Di Giacomi>
2016-11-23 20:37:09 -0500

    updated readme.sh

--- ElasticSearch-Install-RH/runme.sh
+++ ElasticSearch-Install-RH/runme.sh
@@ -6,8 +6,5 @@ sudo yum -y install git
 #Cloning Repo
 git clone https://github.com/mdigiacomi/Usefull-Scripts.git
 
-#Executing Installation
-cd ./Useful-Bash/ElasticSearch-Install-RH/
-
 #Checking it System uses init or systemd and running apropriate script
-pidof /sbin/init && sudo bash ./Useful-Bash/ElasticSearch-Install-RH/install-init.sh > output.log || sudo bash ./Useful-Bash/ElasticSearch-Install-RH/install-systemd.sh > output.log
+pidof /sbin/init && sudo bash ./Useful-Scripts/ElasticSearch-Install-RH/install-init.sh > output.log || sudo bash ./Useful-Scripts/ElasticSearch-Install-RH/install-systemd.sh > output.log
