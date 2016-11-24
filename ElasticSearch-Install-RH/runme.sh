aa990e5849d27505177c07cacbb846ecd176c59a
Michael Di Giacomi <Michael Di Giacomi>
2016-11-23 20:39:07 -0500

    typo

--- ElasticSearch-Install-RH/runme.sh
+++ ElasticSearch-Install-RH/runme.sh
@@ -7,4 +7,4 @@ sudo yum -y install git
 git clone https://github.com/mdigiacomi/Usefull-Scripts.git
 
 #Checking it System uses init or systemd and running apropriate script
-pidof /sbin/init && sudo bash ./Useful-Scripts/ElasticSearch-Install-RH/install-init.sh > output.log || sudo bash ./Useful-Scripts/ElasticSearch-Install-RH/install-systemd.sh > output.log
+pidof /sbin/init && sudo bash ./Usefull-Scripts/ElasticSearch-Install-RH/install-init.sh > output.log || sudo bash ./Usefull-Scripts/ElasticSearch-Install-RH/install-systemd.sh > output.log
